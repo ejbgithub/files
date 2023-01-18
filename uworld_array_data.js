0f8c3de983eefc9c72bffc61871977c6bf9bc0c3
ejbgithub <earljbrown@chartertn.net>
2023-01-18 04:28:38 -0500

    Add files via upload

---
+++ uworld_array_data.js
@@ -0,0 +1,98 @@
+//alert("here");
+
+var myUWorldQuestionsDataArray = [
+    {
+        "varUWquestionsID": "Question Id: 448",
+        "text": "A 64-year-old man has had difficulty walking for 6 months, experiencing muscle cramps in his right thigh after walking a block on level ground.  The cramps subside quickly with rest.  He has also had decreased sexual performance.  Medical history is significant for hypercholesterolemia and myocardial infarction.  The patient smokes a pack of cigarettes daily but does not use alcohol or illicit drugs.  He is afebrile.  Blood pressure is 128/78 mm Hg, pulse is 76/min, and respirations are 16/min.  Cardiac auscultation reveals normal S1 and S2, and equal vesicular breath sounds are heard over the bilateral lungs.  The abdomen is soft and without masses.  There is no peripheral edema, muscle tenderness, or muscle weakness.  Which of the following is the most likely cause of this patient's symptoms?",
+        "disease": "",
+        "videoSettings": {
+            "youtubeID": "",
+            "start": ""
+        }
+    },
+    {
+        "varUWquestionsID": "Question Id: 14989",
+        "text": "A 61-year-old woman is hospitalized due to chest pressure and shortness of breath.  Two days ago her husband died in a car accident.  The patient's medical history is significant for hypertension, hyperlipidemia, and gout.  Five years ago, she was diagnosed with breast cancer and treated with surgery and chemotherapy.  Blood pressure is 165/90 mm Hg and pulse is 95/min and regular.  Physical examination is unremarkable.  ECG shows normal sinus rhythm with T-wave inversions in the anterior leads.  Echocardiogram shows hypokinesis of the apical wall with decreased left ventricular ejection fraction.  Diagnostic coronary angiography shows no evidence of obstructive coronary artery disease.  The patient is treated medically and discharged home.  Four weeks later, she is seen in the office and has no symptoms.  Repeat echocardiography demonstrates normal left ventricular wall motion and ejection fraction.  Which of the following is the likely cause of this patient's initial presentation?",
+        "disease": "",
+        "videoSettings": {
+            "youtubeID": "",
+            "start": ""
+        }
+    },
+    {
+        "varUWquestionsID": "Question Id: 826",
+        "text": "A 35-year-old man comes to the office for evaluation of red skin lesions under his nipple that were first noticed by his wife.  The lesions are not itchy or painful, and no other family members have skin lesions.  He has not used any new soaps, lotions, or detergents.  The patient has no known medical problems and takes no medications.  He drinks 4-6 beers every night.  He does not use tobacco or illicit drugs.  Body mass index is 32kg/m2, and vital signs are normal.  Skin findings are shown below.\n\nWhich of the following is the most likely diagnosis?",
+        "disease": "",
+        "videoSettings": {
+            "youtubeID": "",
+            "start": ""
+        }
+    },
+    {
+        "varUWquestionsID": "Question Id: 1937",
+        "text": "A 54-year-old woman comes to the office because of multiple firm, violaceous nodules on her right upper arm.  She was diagnosed with right breast carcinoma 12 years ago and treated with radical mastectomy.  Extensive axillary lymph node dissection performed at that time revealed no lymph node metastases; the patient developed postoperative chronic lymphedema involving her right arm.  Which of the following is the most likely diagnosis of this patient's skin lesions?",
+        "disease": "",
+        "videoSettings": {
+            "youtubeID": "",
+            "start": ""
+        }
+    },
+    {
+        "varUWquestionsID": "Question Id: 447",
+        "text": "A 44-year-old man is evaluated in the clinic for occasional chest discomfort that is not consistently related to exertion.  The patient's past medical history is significant for hypertension and hyperlipidemia.  His grandfather experienced a myocardial infarction at age 50.  Coronary CT angiography reveals several nonobstructive atherosclerotic plaques in the coronary arteries.  One plaque in the proximal left anterior descending artery appears extensive, has a large hypodense core, and occupies 40% of the lumen.  No intervention is performed.  One year later, the patient comes to the emergency department with acute severe chest pain and is found to have thrombotic occlusion of the proximal left anterior descending artery.  High intraplaque activity of which of the following enzymes most likely resulted in this patient's myocardial infarction?",
+        "disease": "",
+        "videoSettings": {
+            "youtubeID": "",
+            "start": ""
+        }
+    },
+    {
+        "varUWquestionsID": "Question Id: 176",
+        "text": "A 54-year-old man comes to the emergency department with retrosternal chest pain of 30 minutes duration.  The patient also complains of sweating and mild dyspnea.  A single tablet of nitroglycerin is delivered sublingually, and the patient's pain decreases significantly.  The patient has experienced several similar episodes of pain over the last 12 hours, all of which resolved spontaneously.  Which of the following ultrastructural changes would most likely indicate irreversible myocardial cell injury in this patient?",
+        "disease": "",
+        "videoSettings": {
+            "youtubeID": "",
+            "start": ""
+        }
+    },
+    {
+        "varUWquestionsID": "Question Id: 37",
+        "text": "A 55-year-old man is brought to the emergency department due to severe substernal chest pain that is accompanied by sweating and mild dyspnea.  The pain started as midline chest discomfort several hours ago but then progressively worsened such that the patient could not fall asleep.  He received sublingual nitroglycerin in the ambulance without significant pain relief.  His medical history includes hypertension, type 2 diabetes mellitus, and hyperlipidemia.  ECG demonstrates normal sinus rhythm and ST-segment elevation in leads I, aVL, and V1-V4, with deep Q wave development over the next several hours.  Cardiac catheterization in this patient would most likely show which of the following?",
+        "disease": "",
+        "videoSettings": {
+            "youtubeID": "",
+            "start": ""
+        }
+    },
+    {
+        "varUWquestionsID": "Question Id: 19380",
+        "text": "A 17-year-old girl is brought to the emergency department due to hemoptysis and severe respiratory distress.  The patient has been on several oral antibiotics for pneumonia over the past week and has required numerous similar treatments in the past.  She has been coughing up blood for the past day, which has become progressively worse.  Temperature is 37.0 C (98.6 F), blood pressure is 90/60 mm Hg, pulse is 120/min, and respirations are 42/min.  Pulmonary examination reveals diffusely reduced air flow, rales, and intercostal retractions.  Copious red blood is produced while coughing during the examination and progresses rapidly.  Despite aggressive management, the patient ultimately expires.  An autopsy is performed and a gross lung specimen is shown in the image below:\n\nWhich of the following is the most likely etiology of this patient's hemoptysis?",
+        "disease": "",
+        "videoSettings": {
+            "youtubeID": "",
+            "start": ""
+        }
+    },
+    {
+        "varUWquestionsID": "Question Id: 491",
+        "text": "A 35-year-old man comes to the office due to worsening shortness of breath.  The patient is an avid hiker; he began to have shortness of breath 18 months ago on challenging hikes, but it now occurs even while he is walking.  He has also had mild wheezing and sputum production recently.  He has no other medical problems.  The patient has smoked half a pack of cigarettes daily for the past 5 years.  His father died from lung and liver disease at a young age.  Spirometry shows decreased forced expiratory volume in 1 second (FEV1), decreased forced vital capacity (FVC), and a decreased FEV1/FVC ratio.  This patient's lower lung lobes are most likely to demonstrate which of the following findings?",
+        "disease": "",
+        "videoSettings": {
+            "youtubeID": "",
+            "start": ""
+        }
+    },
+    {
+        "varUWquestionsID": "Question Id: 1312",
+        "text": "A 56-year-old man with advanced AIDS comes to the emergency department due to several weeks of progressive fever, night sweats, weight loss, diarrhea, and generalized weakness.  Physical examination shows mucosal pallor, generalized lymphadenopathy, and hepatosplenomegaly.  Laboratory studies reveal anemia and elevated alkaline phosphatase and lactate dehydrogenase levels.  CD4 count is 16/mm3.  Bone marrow biopsy shows aggregates of epithelioid histiocytes with foamy cytoplasm and numerous acid-fast intracellular organisms.  Nucleic acid amplification test of the sample for Mycobacterium tuberculosis is negative.  Which of the following pathogens is the most likely cause of this patient's illness?",
+        "disease": "",
+        "videoSettings": {
+            "youtubeID": "",
+            "start": ""
+        }
+    }
+]
+
+
+console.log(myUWorldQuestionsDataArray);
+
